08d87667d3acfcd6c387cd00877630fd57b036f4
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-03-25 18:13:58 -0300

    inverter_variaveis

---
+++ inverter_variaveis.py
@@ -0,0 +1,11 @@
+print("")
+print("Olá, este programa inverte os conteúdos inseridos?")
+print("")
+var1 = input("Digite o conteúdo da variável 1:")
+print("")
+var2 = input("Digite o conteúdo da variável 2:")
+print("")
+inversor = var1
+var1 = var2
+var2 = inversor
+print("Invertemos os conteúdos inseridos nas duas variáveis, agora temos:\n \nVariável 1 com {}\n \nVariável 2 com {}\n".format(var1, var2))
