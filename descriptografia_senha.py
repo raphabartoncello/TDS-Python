e9aba6c19414f075712580ca9f299f3e0a9c3659
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-03-26 20:03:25 -0300

    descriptografia_senha com importação de minutos

--- descriptografia_senha.py
+++ descriptografia_senha.py
@@ -1,20 +1,17 @@
 print("\nDescriptografia de senha\n")
 
-from datetime import datetime
-agora = datetime.now()
-print ("Agora é {}".format(agora))
+from datetime import timezone, datetime
+agora = datetime.now(tz=timezone.utc)
+print("O minuto atual é: {}.".format(agora.strftime("%M")))
 
-min = int(input("\nInforme qual o minuto atual (hora acima): "))
+min = int(input("\nInforme qual o minuto atual (acima): "))
 
 #calculando o fatorial
 contador = min
 fatorial = 1
-print("\nO valor de {}! é:\n".format(contador))
+
 while contador > 0:
-    print("{} ".format(contador), end='')
-    print("x " if contador > 1 else " = ", end='')
     fatorial = fatorial * contador
-    print("{}".format(fatorial))
     contador = contador - 1
 
 palavra = "LIBERDADE"
